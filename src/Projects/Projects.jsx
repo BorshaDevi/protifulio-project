3cfe6c6fd27e6d02a38710764cde4d09a107fc53
unknown <borshadevi27@gmail.com>
2024-10-24 22:29:14 +0600

    add Projects secation

--- src/Projects/Projects.jsx
+++ src/Projects/Projects.jsx
@@ -1,8 +1,40 @@
 import datas from'../json'
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 const Projects=()=>{
+    useEffect(()=>{
+        AOS.init();
+      },[])
     console.log(datas)
     return(
         <>
+        <div id='Project' className='mt-10 '>
+        <h1 className="font-serif font-bold text-xl text-center text-white " data-aos="fade-up">__My Projects</h1>
+        <div className=' grid lg:grid-cols-3 md:ml-10 grid-cols-1 ml-3 relative'>
+           {
+            datas.map(data => <div key={data.id} className="card bg-base-100 w-96 shadow-xl mt-5">
+                <figure>
+                  <img
+                    src={data.image}
+                    alt="Shoes" />
+                </figure>
+                <div className="card-body absolute  opacity-0 hover:opacity-100 duration-1000 hover:duration-1000 hover:bg-cyan-800 hover:mt-32 hover:rounded-l-lg z-10">
+                  <h2 className="card-title text-cyan-500">
+                    {data.title}
+                   
+                  </h2>
+                  <p className='text-white'>{data.description.slice(0,200)}...</p>
+                  <div className="card-actions justify-end text-cyan-300">
+                    <div className="border p-1 hover:text-cyan-100"><a href={data.liveLink}>Live Link</a></div>
+                    <div className="border p-1 hover:text-cyan-100"><a href={data.githubLink}>Client Github</a></div>
+                    <div className="border p-1 hover:text-cyan-100"><a href={data.servesitGitHubLink}>Sever Github</a></div>
+                  </div>
+                </div>
+              </div>)
+           }
+        </div>
+        </div>
        
         </>
     )
--- src/json.js
+++ src/json.js
@@ -1,12 +1,5 @@
 const  datas=[
-    {
-        id:1,
-        title:'Luxury real estate',
-        description:'Our project redefines sophistication and elegance, delivering an unparalleled luxury experience for discerning users. With a sleek, intuitive interface and a focus on high-end design, we blend modern technology with timeless elegance. From seamless navigation to exclusive features, every detail is crafted to exude excellence, ensuring a premium experience that meets the highest standards of style, comfort, and functionality. Experience luxury, reimagined for the digital age.',
-        liveLink:'https://luxury-real-project.web.app/',
-        githubLink:'https://github.com/BorshaDevi/luxery-project-more-me',
-         image:'https://i.ibb.co.com/YpknTDX/Screenshot-15.png'
-    },
+    
     {
         id:1,
         title:'Medicine E-commerce',
@@ -15,6 +8,24 @@ const  datas=[
         servesitGitHubLink:'https://github.com/BorshaDevi/last-server-medicine-project',
         githubLink:'https://github.com/BorshaDevi/last-client-medicine-project',
          image:'https://i.ibb.co.com/6PjGq0g/Screenshot-16.png'
+    },
+    
+    {
+        id:2,
+        title:'Time To Travel',
+        description:'"Time to TraveL" is an innovative platform designed to streamline travel planning for users exploring destinations across the country. It offers a comprehensive suite of features, including destination discovery, personalized itineraries, and seamless booking options, all within a user-friendly interface.The platform is built with modern web technologies, leveraging Next.js for a fast, dynamic front-end, Node.js and Express.js for efficient back-end operations, and MongoDB for secure data storage. It also integrates with third-party APIs to offer real-time weather updates, booking services, and interactive maps, enhancing the overall travel experience.By providing detailed travel guides, user reviews, and customizable itineraries, "Time to TraveL" aims to make travel planning easy, enjoyable, and accessible. The platform promotes local tourism by showcasing hidden gems and unique destinations across the country, allowing users to explore beyond the usual tourist spots.',
+        liveLink:'https://country-project-1016e.web.app/',
+        servesitGitHubLink:'https://github.com/BorshaDevi/server-site-country-project',
+        githubLink:'https://github.com/BorshaDevi/country-site-project',
+         image:'https://i.ibb.co.com/xSd3KQm/Screenshot-30.png'
+    },
+    {
+        id:3,
+        title:'Luxury real estate',
+        description:'Our project redefines sophistication and elegance, delivering an unparalleled luxury experience for discerning users. With a sleek, intuitive interface and a focus on high-end design, we blend modern technology with timeless elegance. From seamless navigation to exclusive features, every detail is crafted to exude excellence, ensuring a premium experience that meets the highest standards of style, comfort, and functionality. Experience luxury, reimagined for the digital age.',
+        liveLink:'https://luxury-real-project.web.app/',
+        githubLink:'https://github.com/BorshaDevi/luxery-project-more-me',
+         image:'https://i.ibb.co.com/ZYWDCRX/Screenshot-31.png'
     },
 ]
 export default datas;
